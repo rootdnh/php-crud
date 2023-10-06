13caf7bf43a3b7394b58b8313b787757b791a941
rootdnh <rootdnh@gmail.com>
2023-10-06 20:52:45 -0300

    chore: add more configs to database connection

--- DbConnection.php
+++ DbConnection.php
@@ -10,7 +10,13 @@ class DatabaseConnection
     $host = "localhost";
     $db_name = "blog";
 
-    self::$db_connection = new PDO("$db_driver:host=$host;dbname=$db_name", $user, $pass);
+    try{
+      self::$db_connection = new PDO("$db_driver:host=$host;dbname=$db_name", $user, $pass);
+      self::$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      self::$db_connection->exec('SET NAMES utf8');
+    }catch(PDOException $err) {
+      die("Connection Error: " . $err->getMessage());
+    }
   }
   
   public static function connection(){
