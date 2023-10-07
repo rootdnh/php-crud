34f13a932a0c0918545579200f2a6ca5d926a418
rootdnh <rootdnh@gmail.com>
2023-10-06 22:27:12 -0300

    feat: create insert user

--- views/createUser.php
+++ views/createUser.php
@@ -1,9 +1,3 @@
-<?php
-require_once '../DbConnection.php';
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,8 +8,50 @@ require_once '../DbConnection.php';
 </head>
 
 <body>
-  olá
-  <button onclick="javascript:history.go(-1)">voltar</button>
+  <?php
+  require_once "../DbConnection.php";
+  $message = "";
+
+  try {
+
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+    if (isset($name, $email)) {
+      $sql = $con->prepare("INSERT INTO clientes (name, email) VALUES(:name, :email)");
+      $sql->bindValue(":name", $name);
+      $sql->bindValue(":email", $email);
+
+      if ($sql->execute()) {
+        $message = "Usuário criado com sucesso";
+      }else{
+        $message = "Erro ao tentar criar usuário";
+      }
+    }
+  } catch (Exception $err) {
+    if($err->getCode() == 23000){
+      $message = "Este email já está em uso";
+    }else{
+      echo "Erro ao tentar criar usuário" . $err->getMessage();
+    }
+  }
+
+
+  ?>
+  <main>
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+      <input name="name" type="text" placeholder="Digite o nome" value="<?= $name ? $name : '' ?>" required /><br />
+      <input name="email" type="email" placeholder="Digite o email" value="<?= $email ? $email : '' ?>" required /><br />
+      <input type="submit" value="Criar">
+    </form>
+    <?php
+    if (!empty($message)) {
+      echo "<p>$message</p>";
+    }
+    ?>
+  </main>
+
+  <button style="margin-top: 30px" onclick="javascript:window.location.href = '/'">voltar</button>
 </body>
 
 </html>
